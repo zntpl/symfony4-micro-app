80b306396054132f8070c2436008c4c8c7bd70bb
Vitaliy <theyamshikov@yandex.ru>
2020-10-27 22:43:02 +0600

    upd

--- src/Bus/Domain/Repositories/Conf/ProcedureConfigRepository.php
+++ src/Bus/Domain/Repositories/Conf/ProcedureConfigRepository.php
@@ -2,13 +2,26 @@
 
 namespace App\Bus\Domain\Repositories\Conf;
 
+use App\Bus\Domain\Entities\HandlerEntity;
+use App\Bus\Domain\Services\TestService;
+use ZnCore\Base\Legacy\Yii\Helpers\ArrayHelper;
+use ZnCore\Domain\Helpers\EntityHelper;
+
 class ProcedureConfigRepository
 {
 
-
-    function getHandlerByName(string $name): array
+    public function getHandlerByName(string $name): HandlerEntity
     {
-
+        $procedureMap = [
+            'testMethod' => [
+                'serviceClass' => TestService::class,
+                'method' => 'testMethod',
+            ],
+        ];
+        $handler = ArrayHelper::getValue($procedureMap, $name);
+        if (!$handler) {
+            $handler = ArrayHelper::getValue($procedureMap, 'default');
+        }
+        return EntityHelper::createEntity(HandlerEntity::class, $handler);
     }
-
 }
--- src/Bus/Domain/Services/ProcedureService.php
+++ src/Bus/Domain/Services/ProcedureService.php
@@ -3,43 +3,34 @@
 namespace App\Bus\Domain\Services;
 
 use App\Bus\Domain\Entities\HandlerEntity;
+use App\Bus\Domain\Repositories\Conf\ProcedureConfigRepository;
 use Illuminate\Container\Container;
-use ZnCore\Base\Legacy\Yii\Helpers\ArrayHelper;
-use ZnCore\Domain\Helpers\EntityHelper;
 
 class ProcedureService
 {
 
     private $container;
+    private $procedureConfigRepository;
 
     public function __construct(
-        Container $container
+        Container $container,
+        ProcedureConfigRepository $procedureConfigRepository
     )
     {
         $this->container = $container;
+        $this->procedureConfigRepository = $procedureConfigRepository;
     }
 
-    public function run(string $procedureName, array $params) {
-        $handler = $this->getHandler($procedureName);
+    public function run(string $procedureName, array $params)
+    {
+        $handler = $this->procedureConfigRepository->getHandlerByName($procedureName);
+        //$handler = $this->getHandler($procedureName);
         return $this->runProcedure($handler, $params);
     }
 
-    private function runProcedure(HandlerEntity $handlerEntity, array $params) {
+    private function runProcedure(HandlerEntity $handlerEntity, array $params)
+    {
         $serviceInstance = $this->container->get($handlerEntity->getServiceClass());
         return $this->container->call([$serviceInstance, $handlerEntity->getMethod()], $params);
-    }
-
-    private function getHandler(string $name): HandlerEntity {
-        $procedureMap = [
-            'testMethod' => [
-                'serviceClass' => TestService::class,
-                'method' => 'testMethod',
-            ],
-        ];
-        $handler = ArrayHelper::getValue($procedureMap, $name);
-        if( ! $handler) {
-            $handler = ArrayHelper::getValue($procedureMap, 'default');
-        }
-        return EntityHelper::createEntity(HandlerEntity::class, $handler);
     }
 }
